b9a9c461276086b3e8b90173c59f9be93ad6018a
David Albert <davidbalbert@gmail.com>
2020-02-01 16:03:45 -0500

    Initial Commit

---
+++ Service Tester/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Service Tester
+//
+//  Created by David Albert on 2/1/20.
+//  Copyright © 2020 David Albert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Service Tester/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Service Tester
+//
+//  Created by David Albert on 2/1/20.
+//  Copyright © 2020 David Albert. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
